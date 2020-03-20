2d922e9fe922f6da88d5a0882d8c3e3086117a3c
Soufianetyk1337 <sz.fanel@gmail.com>
2020-03-20 20:12:13 +0100

    Adding index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+<section class="hero-area bg-img bg-overlay-2by5" style="background-image: url(<?php echo bloginfo('template_url')?>/img/bg-img/bg1.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <!-- Hero Content -->
+                    <div class="hero-content text-center">
+                        <h2>Let's Study Together</h2>
+                        <a href="#" class="btn clever-btn">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php get_footer();?>
