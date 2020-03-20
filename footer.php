28addc7500ac5f8aa810c908e9041c249d69136b
Reda Abdel_Hakmi <abdelhakmireda37@gmail.com>
2020-03-20 19:23:21 +0100

    Adding Header/Footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="footer-area">
+        <div class="top-footer-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="footer-logo">
+<a href="index.html"><img src="<?php echo bloginfo('template_url')?>/img/core-img/elogo.png" alt=""></a>
+                        </div>
+                        <p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> IHM Template </p>
+                   </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer-area d-flex justify-content-between align-items-center">
+            <div class="contact-info">
+                <a href="#"><span>Phone:</span> +212 6 10 20 30 40</a>
+                <a href="#"><span>Email:</span> estsb@ucd.ma</a>
+            </div>
+            <div class="follow-us">
+                <span>Follow us</span>
+                    <a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </footer>
+    <? wp_footer();?>
+</body>
+​
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+​
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <? wp_head();?>
+​
+    <title>EST Sidi Bennour</title>
+    <link rel="icon" href="<?php echo bloginfo('template_url')?>/img/core-img/favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Raleway:300,400,500,600,700">
+​
+</head>
+​
+<body>
+​
+    <header class="header-area">
+
+        <div class="clever-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <nav class="classy-navbar justify-content-between" id="cleverNav">
+​
+                    <a class="nav-brand" href="index.html"><img src="<?php echo bloginfo('template_url');?>/img/core-img/elogo2.png" alt=""></a>
+​
+                    <!-- Navbar Toggler -->
+                    <div class="classy-navbar-toggler">
+                        <span class="navbarToggler"><span></span><span></span><span></span></span>
+                    </div>
+​
+                    <div class="classy-menu">
+​
+                        <div class="classycloseIcon">
+                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                        </div>
+​
+                        <!-- Nav Start -->
+                        <div class="classynav">
+                            
+                            <!-- Search Button -->
+                            <div class="search-area">
+                                <form action="#" method="post">
+                                    <input type="search" name="search" id="search" placeholder="Search">
+                                    <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- Nav End -->
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
