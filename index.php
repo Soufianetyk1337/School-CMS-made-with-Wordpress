39969881216a9eda8b35e464435ccf922b154daa
Soufianetyk1337 <sz.fanel@gmail.com>
2020-03-20 20:39:05 +0100

    Update index

--- index.php
+++ index.php
@@ -12,4 +12,43 @@
             </div>
         </div>
     </section>
+
+    <section class="cool-facts-area section-padding-100-0">
+        <div class="container">
+            <div class="row">
+                <!-- Single Cool Facts Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-cool-facts-area text-center mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <div class="icon">
+                            <img src="<?php echo bloginfo('template_url');?>/img/core-img/star.png" alt="">
+                        </div>
+                        <h2><span class="counter">123</span></h2>
+                        <h5>Dedicated Tutors</h5>
+                    </div>
+                </div>
+
+                <!-- Single Cool Facts Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-cool-facts-area text-center mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <div class="icon">
+                            <img src="<?php echo bloginfo('template_url');?>/img/core-img/events.png" alt="">
+                        </div>
+                        <h2><span class="counter">89</span></h2>
+                        <h5>Scheduled Events</h5>
+                    </div>
+                </div>
+
+                <!-- Single Cool Facts Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-cool-facts-area text-center mb-100 wow fadeInUp" data-wow-delay="1000ms">
+                        <div class="icon">
+                            <img src="<?php echo bloginfo('template_url');?>/img/core-img/earth.png" alt="">
+                        </div>
+                        <h2><span class="counter">56</span></h2>
+                        <h5>Available Courses</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php get_footer();?>
