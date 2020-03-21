21b8d4afa8ae809b2b546f942d175cbfaece8c99
Reda Abdel_Hakmi <abdelhakmireda37@gmail.com>
2020-03-21 17:25:07 +0100

    Instructors page

---
+++ instructors.php
@@ -0,0 +1,248 @@
+<?php
+/*
+Template Name: Insctructors
+*/?>
+<?php get_header();?>
+
+
+   <div class="instructors-video d-flex align-items-center justify-content-center bg-img" style="background-image: url(<?php echo bloginfo('template_url')?>/img/teacher.jpg);">
+        <h2>Be The Best Teacher</h2>
+    </div>
+    <section class="best-tutors-area section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading">
+                        <h3>The Best Tutors in Town</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="tutors-slide owl-carousel">
+
+                        <div class="single-tutors-slides">
+                            
+                            <div class="tutor-thumbnail">
+                                <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                            </div>
+                            
+                            <div class="tutor-information text-center">
+                                <h5>John Doe</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single-tutors-slides">
+                            <div class="tutor-thumbnail">
+                                <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                            </div>
+                            <div class="tutor-information text-center">
+                                <h5>John Doe</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="single-tutors-slides">
+                            
+                            <div class="tutor-thumbnail">
+                                <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                            </div>
+                            
+                            <div class="tutor-information text-center">
+                                <h5>John Doe</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="single-tutors-slides">
+                            
+                            <div class="tutor-thumbnail">
+                                <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                            </div>
+                            
+                            <div class="tutor-information text-center">
+                                <h5>John Doe</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="single-tutors-slides">
+                            
+                            <div class="tutor-thumbnail">
+                                <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                            </div>
+                            
+                            <div class="tutor-information text-center">
+                                <h5>John Doe</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="top-teacher-area section-padding-0-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading">
+                        <h3>Top Teachers in Every Field</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="<?php echo bloginfo('template_url')?>/img/bg-img/t2.png" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Sarah Jane</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer();?>
