fe18d972d412595ea9d599b8db80074428f8a205
Reda Abdel_Hakmi <abdelhakmireda37@gmail.com>
2020-03-21 23:06:28 +0100

    Courses page

---
+++ courses.php
@@ -0,0 +1,180 @@
+<?php
+    /*Template Name:Courses*/
+?>
+<?php get_header();?>
+<div class="breadcumb-area">
+    </div>
+    <div class="clever-catagory bg-img d-flex align-items-center justify-content-center p-3" style="background-image: url(<?php echo bloginfo('template_url')?>/img/bg-img/bg2.jpg);">
+        <h3>Art &amp; Design</h3>
+    </div>
+
+    <section class="popular-courses-area section-padding-100">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="250ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc5.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Programming</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc2.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Management</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc4.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Languages</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="250ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc2.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Management</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc5.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Programming</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc4.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Languages</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="250ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc5.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Programming</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc2.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Management</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <img src="<?php echo bloginfo('template_url')?>/img/bg-img/bc4.jpg" alt="">
+                        
+                        <div class="course-content">
+                            <h4>Languages</h4>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">Sarah Parker</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        
+        </div>
+    </section>
+    <?php get_footer();?>
