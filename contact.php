272b484b562e17a61c4d647b754b19f2a25f15c3
Reda Abdel_Hakmi <abdelhakmireda37@gmail.com>
2020-03-22 20:48:46 +0100

    adding contact page

---
+++ contact.php
@@ -0,0 +1,71 @@
+<?php get_header();?>
+    <section class="hero-area bg-img bg-overlay-2by5" style="opacity:0.4;background-image: url(<?php echo bloginfo('template_url')?>/img/bg-img/bg1.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                      <div class="hero-content text-center">
+                        <h2>Let's Study Together</h2>
+                        <a href="#" class="btn clever-btn">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   <section class="contact-area">
+        <div class="container">
+            <div class="row">
+             <div class="col-12 col-lg-6">
+                    <div class="contact--info mt-50 mb-100">
+                        <h4>Contact Info</h4>
+                        <ul class="contact-list">
+                            <li>
+                                <h6><i class="fa fa-clock-o" aria-hidden="true"></i> Business Hours</h6>
+                                <h6>8:00 AM - 18:00 PM</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-phone" aria-hidden="true"></i> Number</h6>
+                                <h6>+212 6 10 20 30 40</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-envelope" aria-hidden="true"></i> Email</h6>
+                                <h6>estsb@ucd.ac.ma</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-map-pin" aria-hidden="true"></i> Address</h6>
+                                <h6>Jabran Khalil Jabran Avenue B.P 299-24000</h6>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="contact-form">
+                        <h4>Get In Touch</h4>
+                        <form action="#" method="post">
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="text" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" id="email" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn clever-btn w-100">Send A Message</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php get_footer();?>
+<?php get_footer();?>
